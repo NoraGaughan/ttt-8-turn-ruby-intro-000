efded011e7b9910908b1e67b895a817e9410ac7c
Nora Gaughan <noragaughan@gmail.com>
2020-06-15 18:32:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,3 +28,38 @@ def position_taken? (board, index)
     return true 
   end
 end
+
+def valid_move?(board, position)
+	spot = position.to_i - 1
+	if !position_taken?(board, spot) && spot.between?(0,8)
+		false 
+
+	elsif 
+		false
+
+	end
+
+end
+
+def valid_move?(board, position)
+	spot = position.to_i - 0
+	if !position_taken?(board, spot) && spot.between?(0,8)
+		true  
+
+	elsif 
+		false
+
+	end
+
+end
+
+def position_taken?(board, position)
+  if board[position] == " " || board[position] == "" || board[position] == nil
+  	false
+
+  else
+  	true
+
+  end
+
+end 
