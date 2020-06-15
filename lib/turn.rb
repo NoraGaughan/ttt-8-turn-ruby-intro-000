26f9d93982bda1a25826173b88439d306da5345d
Nora Gaughan <noragaughan@gmail.com>
2020-06-15 18:28:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,3 +20,11 @@ def move(board, index, character = "X")
   board[index] = character
   return board
 end
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    return false 
+  else 
+    return true 
+  end
+end
