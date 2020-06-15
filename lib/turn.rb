58f2b4de80e46fb1d1e7c108f8def12e1e63dfae
Nora Gaughan <noragaughan@gmail.com>
2020-06-15 18:27:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,13 @@ puts "#{lines}"
 puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
 
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
